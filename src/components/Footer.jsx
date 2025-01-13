c74e6482e7a0603fca99d08a425170c3a9a18bac
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-01-13 16:57:50 +0100

    fit: first commit and header main footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class="footer-links">
+        <div class="column">
+          <h3>DC COMICS</h3>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Characters</a></li>
+          </ul>
+        </div>
+        <div class="column">
+          <h3>DC</h3>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Terms Of Use</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+
+
+const Main = () => {
+  return (
+    <main>
+      <div class="content-placeholder">
+        <p>--Content goes here--</p>
+      </div>
+      <div class="blue-section">
+        <div class="icon-links">
+          <a href="#"><img src="" alt="Digital Comics">DIGITAL COMICS</img></a>
+          <a href="#"><img src="" alt="Digital Comics">DIGITAL COMICS</img></a>
+          <a href="#"><img src="" alt="Digital Comics">DIGITAL COMICS</img></a>
+          <a href="#"><img src="" alt="Digital Comics">DIGITAL COMICS</img></a>
+          <a href="#"><img src="" alt="Digital Comics">DIGITAL COMICS</img></a>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Main
